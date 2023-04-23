7f18791a44f7c057dba5902400f52bbe32ec5e94
Gizem <109113608+giizzz@users.noreply.github.com>
2023-04-24 01:38:13 +0300

    %s , %d , %f

---
+++ printFfonk.java
@@ -0,0 +1,9 @@
+public class printFfonk {
+    public static void main(String[] args) {
+        String ad ="Melih Aslan";
+        int yas = 25;
+        double kilo = 85.7;
+        double boy = 1.87;
+        System.out.printf("%s %.2f %d %.1f ",ad,kilo,yas,boy);
+    }
+}
